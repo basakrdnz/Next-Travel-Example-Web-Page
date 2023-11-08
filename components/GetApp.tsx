c5e6bdab3b3edbd85f21872a14207d2b0a33db7a
basakrdnz <84276926+basakrdnz@users.noreply.github.com>
2023-11-08 15:50:44 +0300

    Made the GetApp part

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
-  )
-}
+    <section className="flex flex-col ">
+      <div className="get-app flex flex-col md:flex-row md:max-h-[600px]">
+        <div className="m-auto ">
+          <h2 className="bold-40 text-white lg:bold-64 lg:max-w-[250px] xl:max-w-[320px]">
+            Get for free now!
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on İOS and Android
+          </p>
+          <div className="mt-10 flex flex-col md:flex-row gap-10 whitespace-nowrap w-full">
+            <Button
+              type={"button"}
+              title="App Store"
+              variant="btn_white"
+              icon="/apple.svg"
+              full
+            ></Button>
+            <Button
+              type={"button"}
+              title="Play Store"
+              variant="btn_dark_green_outline"
+              icon="/android.svg"
+              full
+            ></Button>
+          </div>
+        </div>
+        <div className="md:-mt-40 -mt-20">
+          <Image
+            src="/phones.png"
+            alt="phone"
+            height={870}
+            width={550}
+            className="h-auto"
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default GetApp
+export default GetApp;
